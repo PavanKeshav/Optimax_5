6c6ff7e20253afdb5e45b1ef7659ab5cc6b6a112
DroidManPK <pavankesha242@outlook.com>
2016-10-16 18:18:45 +0530

    Core Memory Optimizations and added hell lot of cpu governors

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#9 SMP PREEMPT Sun Oct 16 16:46:40 IST 2016"
+#define UTS_VERSION "#12 SMP PREEMPT Sun Oct 16 18:12:13 IST 2016"
 #define LINUX_COMPILE_BY "pavan"
 #define LINUX_COMPILE_HOST "pavan-ROG550JK"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.61-StockOptimized+"
+#define UTS_RELEASE "3.10.61-Optimax_5.5+"
