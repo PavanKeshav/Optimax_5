e3aa19a0b1577e5af308d2d5670c8e571d8ba705
DroidManPK <pavankesha242@outlook.com>
2016-10-16 11:31:10 +0530

    Stock kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Oct 16 10:44:12 IST 2016"
+#define LINUX_COMPILE_BY "pavan"
+#define LINUX_COMPILE_HOST "pavan-ROG550JK"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
