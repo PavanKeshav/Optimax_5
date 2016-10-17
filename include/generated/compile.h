72f59b778a71e14a8f534dafea0e8829d1bb1370
DroidManPK <pavankesha242@outlook.com>
2016-10-17 21:28:50 +0530

    Selinux Permissve COmplete with Sdcard access fixed

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#15 SMP PREEMPT Mon Oct 17 19:45:40 IST 2016"
+#define UTS_VERSION "#16 SMP PREEMPT Mon Oct 17 21:15:13 IST 2016"
 #define LINUX_COMPILE_BY "pavan"
 #define LINUX_COMPILE_HOST "pavan-ROG550JK"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.61-Optimax5.6+"
+#define UTS_RELEASE "3.10.61-Optimax5.7+"
