92ed4e68db739a263060e67e19bba4336937a1f5
DroidManPK <pavankesha242@outlook.com>
2016-10-17 21:10:46 +0530

    Making Selinux permissive

--- security/selinux/exports.c
+++ security/selinux/exports.c
@@ -19,11 +19,11 @@
 
 bool selinux_is_enabled(void)
 {
-#ifdef CONFIG_ALWAYS_ENFORCE
-	return true;
-#else
+//#ifdef CONFIG_ALWAYS_ENFORCE
+	//return true;
+//#else
 	return selinux_enabled;
-#endif
+//#endif
 }
 EXPORT_SYMBOL_GPL(selinux_is_enabled);
 
--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -151,11 +151,11 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
-#ifdef CONFIG_ALWAYS_ENFORCE
-		selinux_enforcing = 1;
-#else
+//#ifdef CONFIG_ALWAYS_ENFORCE
+	//	selinux_enforcing = 1;
+//#else
 		selinux_enforcing = enforcing ? 1 : 0;
-#endif
+//#endif
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
@@ -168,11 +168,11 @@ static int __init selinux_enabled_setup(char *str)
 {
 	unsigned long enabled;
 	if (!strict_strtoul(str, 0, &enabled))
-#ifdef CONFIG_ALWAYS_ENFORCE
-		selinux_enabled = 1;
-#else
+//#ifdef CONFIG_ALWAYS_ENFORCE
+		//selinux_enabled = 1;
+//#else
 		selinux_enabled = enabled ? 1 : 0;
-#endif
+//#endif
 	return 1;
 }
 __setup("selinux=", selinux_enabled_setup);
@@ -5295,11 +5295,11 @@ static int selinux_nlmsg_perm(struct sock *sk, struct sk_buff *skb)
 				  "SELinux:  unrecognized netlink message"
 				  " type=%hu for sclass=%hu\n",
 				  nlh->nlmsg_type, sksec->sclass);
-#ifdef CONFIG_ALWAYS_ENFORCE
-			if (security_get_allow_unknown())
-#else
+//#ifdef CONFIG_ALWAYS_ENFORCE
+		//	if (security_get_allow_unknown())
+//#else
 			if (!selinux_enforcing || security_get_allow_unknown())
-#endif
+//#endif
 				err = 0;
 		}
 
@@ -6712,11 +6712,11 @@ static struct security_operations selinux_ops = {
 static __init int selinux_init(void)
 {
 	if (!security_module_enable(&selinux_ops)) {
-#ifdef CONFIG_ALWAYS_ENFORCE
-		selinux_enabled = 1;
-#else
+//#ifdef CONFIG_ALWAYS_ENFORCE
+//		selinux_enabled = 1;
+//#else
 		selinux_enabled = 0;
-#endif
+//#endif
 		return 0;
 	}
 
@@ -6739,9 +6739,9 @@ static __init int selinux_init(void)
 
 	if (register_security(&selinux_ops))
 		panic("SELinux: Unable to register with kernel.\n");
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
+//#ifdef CONFIG_ALWAYS_ENFORCE
+	//selinux_enforcing = 1;
+//#endif
 	if (selinux_enforcing)
 		printk(KERN_DEBUG "SELinux:  Starting in enforcing mode\n");
 	else
@@ -6818,9 +6818,9 @@ static struct nf_hook_ops selinux_ipv6_ops[] = {
 static int __init selinux_nf_ip_init(void)
 {
 	int err = 0;
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enabled = 1;
-#endif
+//#ifdef CONFIG_ALWAYS_ENFORCE
+	//selinux_enabled = 1;
+//#endif
 	if (!selinux_enabled)
 		goto out;
 
