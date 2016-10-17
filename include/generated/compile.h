59f1019297ad563a8ad0bcb09725d0576c7472bf
DroidManPK <pavankesha242@outlook.com>
2016-10-17 20:52:58 +0530

    Attempt to fix bugs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 15 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#12 SMP PREEMPT Sun Oct 16 18:12:13 IST 2016"
+#define UTS_VERSION "#15 SMP PREEMPT Mon Oct 17 19:45:40 IST 2016"
 #define LINUX_COMPILE_BY "pavan"
 #define LINUX_COMPILE_HOST "pavan-ROG550JK"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
